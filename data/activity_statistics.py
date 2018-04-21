889f22a571f788c398ac7a02173e39165b092420
aytung94 <aytung94@gmail.com>
2018-04-21 13:28:20 -0500

    created statistics

--- data/activity_statistics.py
+++ data/activity_statistics.py
@@ -6,17 +6,20 @@ from signal import *
 import calendar
 from datetime import datetime
 
-
 csv_to_read = 'tung_hist_jan_mar_weather_nolocomotion_mood'
 df = pd.read_csv(csv_to_read + '.csv', index_col=0)
 
-print df['Name'].value_counts()
+stats_df = df['Name'].value_counts()
+stats_df.to_csv('activity_frequency.csv')
+stats_df = stats_df.to_dict()
+stats_df = sorted(stats_df.items(), key=lambda kv: kv[1], reverse=True)
 
-#while True:
-#	try:
-#		activity = input('')
-#		print  df.groupby(	
-#	except (TypeError, SyntaxError, ValueError, NameError) as e:
-#		print "Enter valid activity"
-#		continue
-#print "\n\n"
+mean_df = df.groupby('Name', as_index=False)['Mood'].mean()
+
+new_df = pd.DataFrame()
+for i in stats_df:	
+	new_df = new_df.append(mean_df.loc[mean_df['Name'] == i[0]])
+new_df.dropna(subset=['Mood'], inplace=True)
+
+new_df.to_csv('average_mood.csv')
+
